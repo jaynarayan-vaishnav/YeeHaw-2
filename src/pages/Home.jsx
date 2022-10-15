ade1eb2106441d2c19168901d9bb365b5ec0e158
zelfroster <sumitkumarsoni123@gmail.com>
2022-10-15 16:46:46 +0530

    resolution adjustment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Navbar from "../Components/Navbar";
 
 function App() {
   return (
-    <div className="bg-main min-h-screen">
+    <div className="bg-main min-h-screen bg-no-repeat bg-cover">
       <Navbar />
     </div>
   )
