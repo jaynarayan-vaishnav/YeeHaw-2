c4af3355b7f3e5bf70d43e5d85d35b67f6e21c24
Rakesh Sangem <107752425+RakeshSangem@users.noreply.github.com>
2022-10-16 01:41:49 +0530

    created shop page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, HashRouter } from 'react-router-dom';
 import Home from "./pages/Home";
 import Bounty from "./pages/Bounty"
+import Shop from './pages/Shop';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
 					<Routes>
 						<Route path="/" element={<Home />} />
             <Route path="/bounty" element={<Bounty />} />
+            <Route path="/shop" element={<Shop />} />
 					</Routes>
 			</HashRouter>
     </div>
