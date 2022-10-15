e85bd1f34fe206323df7d1090a2366cb40567e41
zelfroster <sumitkumarsoni123@gmail.com>
2022-10-15 16:24:51 +0530

    Setup routing and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import './App.css'
+import { Routes, Route, HashRouter } from 'react-router-dom';
+import Home from "./pages/Home";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <main>
-      <p className="text-2xl font-rye">HuntersNest</p>
-    </main>
+    <div className="min-h-screen">
+      <HashRouter>
+					<Routes>
+						<Route path="/" element={<Home />} />
+					</Routes>
+			</HashRouter>
+    </div>
   )
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,9 @@ module.exports = {
   content: ["./index.html","./src/**/*.{js,jsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        'main': "linear-gradient(to bottom, rgba(77, 43, 53, 0.3), rgba(29, 27, 26, 0.3)), url('/bgimage.jpg')",
+      },
       fontFamily: {
         cormorant: ["Cormorant", "serif"],
         rancho: ["Rancho", "cursive"],
