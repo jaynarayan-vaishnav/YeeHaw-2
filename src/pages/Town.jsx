f46292b31c651c5b63f7b97db54b16a1cb20a0b0
jaynarayan-vaishnav <jaynarayan540@gmail.com>
2022-12-12 20:49:36 +0530

    added search component

--- src/pages/Town.jsx
+++ src/pages/Town.jsx
@@ -1,15 +1,17 @@
 import Navbar from "../Components/Navbar";
 import Towns from "../Components/Towns";
 import cities from "../../content/cities.json";
+import Search from "../Components/Search";
 function Town() {
   return (
     <main>
       <div className="bg-main bg-fixed  min-h-screen bg-no-repeat bg-cover">
         <Navbar />
         <div>
-          <section className=" flex w-[85vw] mx-auto justify">
-            <div className=" overflow-y-scroll h-[60vh] mt-40 w-full">
-              <div className=' flex flex-col gap-10'>
+          <section className=" w-[85vw] mx-auto justify">
+            <Search />
+            <div className=" overflow-y-scroll h-[56vh] mt-36 w-full">
+              <div className=' font-cormorant text-lg flex flex-col gap-10'>
                 {cities.map((item, key) => <Towns key={key} item={item} />)}
               </div>
             </div>
