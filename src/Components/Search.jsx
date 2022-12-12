4aba9d95671f01651c50e40fa3d23f2ae38be547
Jaynarayan vaishnav <jaynarayan540@gmail.com>
2022-12-12 20:55:07 +0530

    Search (#19)
    * Imported Search component
    
    * added search component

---
+++ src/Components/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Search() {
+  return (
+    <div>
+      <input className="relative left-24 top-32 px-[5vw] mx-[26vw] pt-3 pb-4 mb-5 placeholder:Capitalize rounded-lg" type="text" placeholder="Search..." />
+      <div></div>
+    </div>
+  )
+}
+
+export default Search
